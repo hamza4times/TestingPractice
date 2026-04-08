b9acb40824954dc8d376391fc52fc298ea604d09
hamza4times <241655835+hamza4times@users.noreply.github.com>
2026-04-08 10:00:57 -0500

    create capitalize, reverseString functions and calculator Object

--- script.test.js
+++ script.test.js
@@ -1,5 +1,20 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+import {capitalize, reverseString, calculatorObject} from "./script.js"
+
+test('capitalize function', () => {
+  expect(capitalize("hello!")).toBe("Hello!");
+  expect(capitalize("bye bye")).toBe("Bye bye");
+  expect(capitalize("morning!")).toBe("Morning!");
 });
 
-import {sum} from "./script.js"
+test('reverseString function', () => {
+  expect(reverseString("Hello")).toBe("olleH");
+  expect(reverseString("Bye Bye")).toBe("eyB eyB");
+  expect(reverseString("racecar")).toBe("racecar");
+});
+
+test('calculatorObject object functions', () => {
+  expect(calculatorObject.add(10, 20)).toBe(30);
+  expect(calculatorObject.subtract(20, 5)).toBe(15);
+  expect(calculatorObject.multiply(10, 20)).toBe(200);
+  expect(calculatorObject.divide(10, 5)).toBe(2);
+})
